9e4022d38624004a4c1e3d73290946ad7c295eca
adam <chihaadam137@gmail.com>
2025-08-09 06:16:35 +0100

    simple fixes

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,20 @@ import cors from "cors";
 import { PORT, FRONTEND_URL, IS_PRODUCTION } from "./config/env.js";
 import chalk from "chalk";
 import morgan from "morgan";
-import {dbConnection, dbConnectionServerless} from "./config/dbConnection.js";
+import {dbConnection} from "./config/dbConnection.js";
 import usersRouter from "./routes/users.route.js";
 import todoRouter from "./routes/todo.route.js";
 import refreshRouter from "./routes/refresh.route.js";
 import { ErrorHandler } from "./middlewares/errorHandler.middleware.js";
 import cookieParser from "cookie-parser";
+import { dbConnectionServerlessMiddleware } from "./middlewares/dbConnectionServerless.middleware.js";
 const app = express();
 
 app.use(morgan("dev"));
 app.use(cors({ origin: FRONTEND_URL, 
                credentials: true ,
               }));
-if (IS_PRODUCTION) app.use(dbConnectionServerless);
+if (IS_PRODUCTION) app.use(dbConnectionServerlessMiddleware);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/users", usersRouter);
@@ -23,8 +24,9 @@ app.use("/todos", todoRouter);
 app.use("/refresh", refreshRouter);
 app.use(ErrorHandler);
 
-//start server
+
 if (!IS_PRODUCTION){
+  //start server
   app.listen(PORT, async () => {
   console.log(
     chalk.cyan.bold("server is listening on port ") +
