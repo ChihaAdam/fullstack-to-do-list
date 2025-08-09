be5b67ba38113926ae9b06d59f61f382bb6096d2
adam <chihaadam137@gmail.com>
2025-08-09 22:07:11 +0100

    simple fixes

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(morgan("dev"));
-app.use(cors({ origin: FRONTEND_URL, 
+app.use(cors({ origin: '*', 
                credentials: true ,
               }));
 app.use(express.json());
